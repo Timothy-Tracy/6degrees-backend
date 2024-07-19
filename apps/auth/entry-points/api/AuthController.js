c9e1982a02f844ffd1132c99a632ee1b754603c6
NoFocuss <timothydtracy@gmail.com>
2024-07-19 11:38:13 -0700

    eb stuff

--- apps/auth/entry-points/api/AuthController.js
+++ apps/auth/entry-points/api/AuthController.js
@@ -14,18 +14,5 @@ router.post('/verify', catchAsync(AuthService.verify), function (req, res) {
     res.status(200).json(res.locals.auth.tokenData)
 });
 
-// router.get('/register', UserService.findAll, async function (req, res){
-//     res.status(200).json(res.result)
-// });
-
-// router.get('/:UUID', UserService.findOneByUUID, async function (req, res){
-//     console.log("UserController: Finding User By UUID ")
-
-//     res.status(200).json(res.result)
-// });
-
-// router.delete('/', UserService.deleteUser, async function (req, res){
-//     res.status(200).json(res.result)
-// });
 
 module.exports = { apiRoot, router };
