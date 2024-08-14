0f005319321d2ee7a3a6ceae9c67c6c9b1602d9d
NoFocuss <timothydtracy@gmail.com>
2024-08-14 15:02:24 -0700

    kms2

--- apps/edges/domain/EdgeService.js
+++ apps/edges/domain/EdgeService.js
@@ -2,6 +2,7 @@ const { v7: uuidv7 } = require('uuid');
 const EdgeRepository = require('../data-access/EdgeRepository.js');
 const randomWordSlugs = require('random-word-slugs')
 const mylogger = require('../../../lib/logger/logger.js');
+const { AppError, catchAsync } = require('../../../lib/error/customErrors.js');
 const logger = mylogger.child({ 'module': 'EdgeService' }, {options:{name:'EdgeService'}});
 
 
@@ -63,4 +64,104 @@ async function findOneByNodeUUID(NODE_UUID){
 
 }
 
-module.exports = { createDistributionNew,createDistribution, findOneByQuery,findOneByNodeUUID }
+
+async function getUuidByQuery(query, label){
+    const Repository = require('../../db/neo4j/data-access/Repository.js');
+    const log = logger.child({'function':'getUuidByQuery'});
+    log.trace();
+    let result = []
+    let relationship = {
+        type: 'EDGE',
+        properties: {EDGE_QUERY: query}
+
+    }
+    let nodeResult = await Repository.getRel(
+        {
+            label: 'NODE',
+
+        },
+        relationship)
+    
+    let source = nodeResult.data[0].source.properties;
+    let sourceParam = {
+        label: 'NODE',
+        properties: {'NODE_UUID': source.NODE_UUID}
+    }
+
+    if(label == null) throw new AppError('No label provided', 500)
+    switch (label){
+        case 'NODE' : {
+            log.debug('case NODE')
+            result.push([{NODE_UUID : source.NODE_UUID}])
+            break
+        } 
+        case 'POST' : {
+            log.debug('case POST')
+            let postResult = await Repository.getRel(
+                sourceParam,
+                {
+                    type: 'PARENT_POST'
+                },
+                {
+                    label: label,
+        
+                }
+            )
+            log.debug(postResult)
+            let post = postResult.data[0].target.properties;
+            result.push({POST_UUID : post.POST_UUID})
+            break
+        }
+
+        case 'USER' : {
+            let userResult = await Repository.getRel(
+                sourceParam,
+                {
+                    type: 'PARENT_USER'
+                },
+                {
+                    label: label,
+        
+                }
+            )
+            let user = userResult.data[0].target.properties;
+            result.push({USER_UUID : user.USER_UUID})
+            break
+
+        }
+
+        case 'COMMENT' : {
+            let commentResult = await Repository.getRel(
+                sourceParam,
+                {
+                    type: 'PARENT_NODE',
+                    direction:'left'
+                },
+                {
+                    label: label,
+        
+                }
+            )
+          
+            let comments = commentResult.data.map((record)=> (record.target.properties.COMMENT_UUID))
+            result = comments.map((commentUuid) => ({COMMENT_UUID: commentUuid}))
+            break
+        }
+        default:{
+            throw new AppError('switch statement for label default case', 500)
+            break
+        }
+    }
+    log.info(result)
+    return result
+}
+
+
+async function test() {
+    
+    let result =await catchAsync(await getUuidByQuery('silly-gray-microphone', 'COMMENT'))
+    logger.info(result)
+}
+
+//test()
+module.exports = { createDistributionNew,createDistribution, findOneByQuery,findOneByNodeUUID,getUuidByQuery }
