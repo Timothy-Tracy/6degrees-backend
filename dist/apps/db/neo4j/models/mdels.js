d6d13b7740c6a4988c81b6a063f8480001295aef
NoFocuss <timothydtracy@gmail.com>
2024-10-05 17:23:48 -0700

    remove dist

--- dist/apps/db/neo4j/models/mdels.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.models = void 0;
-exports.models = {
-    USER: null,
-    SHARENODE: null,
-};
-//# sourceMappingURL=mdels.js.map
