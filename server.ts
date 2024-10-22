313544b5be7ccd032eba6f8ea93fb667ab59611e
NoFocuss <timothydtracy@gmail.com>
2024-10-22 02:41:25 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -47,8 +47,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: false,
-    httpOnly: false,
+    secure: true,
+    httpOnly: true,
     maxAge: 24*60*60*1000
   }
 }));
